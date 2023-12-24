b5386c5ec6071e0adb8816119c21a26c2762534c
Mohd Arish <arishmd768@gmail>
2023-12-24 18:36:43 +0530

    add cart

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,28 +1,71 @@
-import React from 'react';
+import React, { useState ,useEffect} from 'react';
 import "./style.css";
+import Model from '../Modal/Model';
+import Slider from '../Slider/slider';
 
-const Header = () => {
+
+
+
+const Header = ({updateCount,cart}) => {
+  const [count,setCount]=useState(0);
+  const [isOpen,setIsOpen]=useState(false);
+  const [res,setRes]=useState([]);
+
+
+ 
+  useEffect(() => {
+    // Function to update the cart count
+    const updateCartCount = () => {
+  
+      let map = new Map();
+
+      for (let val of cart) {
+        const key = JSON.stringify(val);
+        if (map.has(key)) {
+          map.set(key, map.get(key) + 1);
+        } else {
+          map.set(key, 1);
+        }
+      }
+
+      const result = [...map.entries()].map(([key, count]) => [JSON.parse(key), count]);
+       setRes(result);
+      
+    
+  
+     
+      const totalCount = cart.length;
+  
+     
+      setCount(totalCount);
+    };
+
+    // Call the function when the component mounts
+    updateCartCount();
+
+    
+  }, [updateCount]);
+
+
+  const handleCartClick=()=>{
+     setIsOpen((prev)=>!prev);
+  }
+
+  
+
+
+
   return (
     <>
       <div className='header'>
-        <h2 className='restaurant-name'>Restaurant</h2>
-        <div className='cart'>
+        <h2 className='restaurant-name'> E-commerce</h2>
+        <div className='cart' onClick={handleCartClick}>
           <div className='add-to-cart-icon'>Cart</div>
-          <div className='count'>0</div>
+          <div className='count'>{count}</div>
         </div>
       </div>
-      <div className='im'>
-        <img src='../../../public/food2.jpeg' className='cover-image' />
-      </div>
-      <div className='outer-desc'>
-        <div className='desc'>
-          <h2>Delicious Food, Delivered to you..</h2><br/><br/>
-          <h4>From mouth-watering appetizers to delectable main courses and divine desserts, 
-            our chefs take pride in using the finest ingredients to deliver flavors
-            that will leave you craving for more </h4>
-            <h4>Planning your visit is made easy with our online reservation system</h4>
-        </div>
-      </div>
+      <Model  isOpen={isOpen}  setRes={setRes} res={res} setCount={setCount} count={count}/>
+     <Slider/>
     </>
   );
 };
--- src/components/Layout/Menu/MenuList.jsx
+++ src/components/Layout/Menu/MenuList.jsx
@@ -3,79 +3,140 @@ import "./style.css";
 
 
 const menuList=[
-    {
-        name:"Pizza",
+    {   id:1,
+        name:"Frok",
         prize:200,
-        img:"../../../../public/pizza.jpeg"
+        img:"../../../../public/cl1.jpeg"
     },
 
-    {
-        name:"Burger",
-        prize:250,
-        img:"../../../../public/burger.jpeg"
+    {   id:2,
+        name:"Jacket",
+        prize:1000,
+        img:"../../../../public/cl2.jpeg"
     },
-    {
-        name:"Cold Drink",
-        prize:100,
-        img:"../../../../public/drink.jpeg"
+    {   id:3,
+        name:"Coat",
+        prize:3000,
+        img:"../../../../public/cl3.jpeg"
     },
-    {
-        name:"Pasta",
-        prize:60,
-        img:"../../../../public/pasta.jpeg"
+    {   id:4,
+        name:"Shirt",
+        prize:500,
+        img:"../../../../public/cl4.jpeg"
     },
    
 
-    {
-        name:"Chawmin",
-        prize:60,
-        img:"../../../../public/chawmin.jpeg"
+    {   id:5,
+        name:"Samsung",
+        prize:50000,
+        img:"../../../../public/mob1.jpeg"
     },
-    {
-        name:"Chaule Bhatore",
-        prize:100,
-        img:"../../../../public/bhatore.jpeg"
+    {   id:6,
+        name:"OnePlus",
+        prize:30000,
+        img:"../../../../public/mob2.jpeg"
     },
-    {
-        name:" French frize",
-        prize:90,
-        img:"../../../../public/french.jpeg"
+    {   id:7,
+        name:" Iphone",
+        prize:100000,
+        img:"../../../../public/mob3.jpeg"
     },
-    {
-        name:"Chicken Legs",
-        prize:200,
-        img:"../../../../public/legs.jpeg"
+    {   id:8,
+        name:"Vivo",
+        prize:25000,
+        img:"../../../../public/mob4.jpeg"
     },
-    {
-        name:"Momos",
-        prize:90,
-        img:"../../../../public/momos.jpeg"
+    {   id:9,
+        name:"Fararee",
+        prize:150000,
+        img:"../../../../public/fararee.jpeg"
     },
-    {
-        name:"Pasti",
-        prize:50,
-        img:"../../../../public/pasti.jpeg"
+    {   id:10,
+        name:"Audi",
+        prize:2000000,
+        img:"../../../../public/audi.jpeg"
     },
-    {
-        name:"Chiecken Roll",
-        prize:120,
-        img:"../../../../public/roll.jpeg"
+    {   id:11,
+        name:"Lambogni",
+        prize:50000000,
+        img:"../../../../public/lambogni.jpeg"
     },
     
-    {
-        name:"Samosa",
-        prize:10,
-        img:"../../../../public/samosa.jpeg"
+    {   id:12,
+        name:"Ford",
+        prize:130000,
+        img:"../../../../public/ford.jpeg"
+    },  
+    {   id:13,
+        name:"Gold Neckles",
+        prize:1500000,
+        img:"../../../../public/gold1.jpeg"
+    },   {   id:14,
+        name:"Diamount Ring",
+        prize:1000000,
+        img:"../../../../public/gold2.jpeg"
+    },   {   id:15,
+        name:"Neckles",
+        prize:120000,
+        img:"../../../../public/gold3.jpeg"
+    },   {   id:16,
+        name:"Bangle",
+        prize:120000,
+        img:"../../../../public/gold4.jpeg"
+    },   {   id:17,
+        name:"Splender",
+        prize:60000,
+        img:"../../../../public/splender.jpeg"
+    },   {   id:18,
+        name:"Sport",
+        prize:1500000,
+        img:"../../../../public/sport.jpeg"
+    },   {   id:19,
+        name:"Yahama",
+        prize:150000,
+        img:"../../../../public/yamah.jpeg"
+    },   {   id:20,
+        name:"KTM",
+        prize:1600000,
+        img:"../../../../public/ktm.jpeg"
+    },   {   id:21,
+        name:"bOAT",
+        prize:1000,
+        img:"../../../../public/pod1.jpeg"
+    },   {   id:22,
+        name:"boAT",
+        prize:800,
+        img:"../../../../public/pod2.jpeg"
+    },   {   id:23,
+        name:"Noise",
+        prize:900,
+        img:"../../../../public/pod3.jpeg"
+    },  
+    {   id:24,
+        name:"Apple",
+        prize:2100,
+        img:"../../../../public/pod4.jpeg"
     },  
 
 
-]
 
-const MenuList = () => {
+];
+
+
+
+
+const MenuList = ({handle,cart,setCart}) => {
+
+    const addToCart=(id)=>{
+ 
+        const data = menuList.find((item) => item.id === id);
+        setCart((prevCart) => [...prevCart, data]);
+        handle();
+    }
   return (
     <div class="item-container">
-    {menuList.map(({ name, prize, img }) => (
-        <div class="item">
+    {menuList.map(({ name, prize, img,id }) => (
+        <div class="item" key={id}>
             <img src={img} alt={name}   />
             <div > 
 
@@ -84,7 +145,7 @@ const MenuList = () => {
           
             <h2> { `Rs ${prize}`}</h2>
                 </div>
-                <div  style={{marginTop:"1rem"}}><button className='btn'>Order</button> </div>
+                <div  style={{marginTop:"1rem"}}><button className='btn'onClick={()=>addToCart(id)}>Order</button> </div>
                 
             </div>
            
